179a1cd5d3e193c3a0cde18a7b216c6081e6f52d
ADITAYA RANA <adityarananee@gmail.com>
2020-07-14 11:53:41 +0200

    Initial Commit

---
+++ Dainik-Karya/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dainik-Karya
+//
+//  Created by Vineet Mahali on 14/07/20.
+//  Copyright © 2020 Aditaya Rana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
