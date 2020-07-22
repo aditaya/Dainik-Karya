855fddeffae3d2ee5b039c73a93a39213b1819fa
ADITAYA RANA <adityarananee@gmail.com>
2020-07-23 00:49:22 +0200

    ToDo List

---
+++ Dainik-Karya/Controllers/DainikViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  Dainik-Karya
+//
+//  Created by Vineet Mahali on 14/07/20.
+//  Copyright © 2020 Aditaya Rana. All rights reserved.
+//
+
+import UIKit
+
+
+class DainikViewController: UITableViewController {
+
+    var itemArray = ["One", "Two", "Three"]
+    let defaults = UserDefaults()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let item = defaults.array(forKey: "dainikArray")  as? [String] {
+            itemArray = item
+        }
+        
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DainikItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+       
+        var textField = UITextField()
+        let alert = UIAlertController(title: " Add new Dainik Karya", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Karya", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.defaults.setValue(self.itemArray, forKey: "dainikArray")
+            self.tableView.reloadData()
+            
+        }
+        
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new Karya"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+       present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+}
+
+
+  
---
+++ Dainik-Karya/Data Model/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  Dainik-Karya
+//
+//  Created by Vineet Mahali on 21/07/20.
+//  Copyright © 2020 Aditaya Rana. All rights reserved.
+//
+
+import Foundation
